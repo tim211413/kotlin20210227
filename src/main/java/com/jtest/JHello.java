86befa2c530f7af7ee05f0eab0e4f34f84bbb0ab
MB-study <tim211413@gmail.com>
2021-02-27 12:24:04 +0800

    Kotlin

---
+++ src/main/java/com/jtest/JHello.java
@@ -0,0 +1,19 @@
+package com.jtest;
+
+import com.ktest.KHello;
+
+public class JHello {
+    public void callMe() {
+        System.out.println("0921443123");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        //呼叫jHello 的 callMe()
+        JHello j = new JHello();
+        j.callMe();
+        //呼叫kHello 的 callme()
+        KHello k = new KHello();
+        k.callMe();
+    }
+}
---
+++ src/main/kotlin/com/ktest/KHello.kt
@@ -0,0 +1,19 @@
+package com.ktest
+
+import com.jtest.JHello
+
+class KHello {
+    fun callMe(){
+        print("pc@school.com")
+    }
+}
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin")
+    //呼叫JHello 的 callMe()
+    val j = JHello()
+    j.callMe()
+    //呼叫\KHello 的 callMe()
+    val k = KHello()
+    k.callMe()
+}
